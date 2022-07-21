9dc911bc48bf2256e844612ad738302395946248
bnun <85907829+bnunowo@users.noreply.github.com>
2022-07-20 20:20:44 -0500

    Add files via upload

---
+++ pac.sh
@@ -0,0 +1,1 @@
+sudo pacman -S i3-gaps i3blocks i3lock git alacritty xorg-server xorg-xinit thunar ttf-font-awesome adobe-source-code-pro-fonts polybar && sudo cp -r i3 ~/.config && sudo cp -r polybar /etc/
